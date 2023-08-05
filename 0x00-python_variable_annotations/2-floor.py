0a2aa8abcca03ac58ce0b8a78dac95d9630f861c
Hope Davids <hopeolawole@gmail.com>
2023-08-05 13:25:55 +0000

    basic annotation - float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+""" Basic annotations - floor. """
+
+def floor(n: float) ->float:
+    """ takes a float n as argument and returns 
+        the floor of the float.
+    """
+    return n
+
