66d8ede56784ae8af7f0bc1572a1d5817bee0806
Hope Davids <hopeolawole@gmail.com>
2023-08-05 13:14:59 +0000

    basic annotaion-concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+""" Basic annotations - concat """
+
+def concat(str1: str, str2: str) -> str:
+    """ This method takes in string annotations
+        and returns string data type.
+    """
+    return str1 + str2
+
